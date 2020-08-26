d96b402d42c802dc1569b43cc60163e8bb05a859
Taraflex <taraflex.red@gmail.com>
2020-08-26 10:28:20 +0300

    add menu component

---
+++ src/ContextMenu.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { onMount, onDestroy } from 'svelte';
+
+    const id = '_menu_' + APP_TITLE;
+    export let label = '';
+    export let icon = APP_ICON;
+    onMount(() => document.body.setAttribute('contextmenu', id));
+    onDestroy(() => document.body.removeAttribute('contextmenu'));
+</script>
+
+<menu type="context" {id}>
+    <menuitem {label} {icon} on:click />
+</menu>
