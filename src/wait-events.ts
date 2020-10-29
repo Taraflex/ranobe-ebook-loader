e59c211cca8248cfe18cd196b73e3679c8d0dcc5
Taraflex <taraflex.red@gmail.com>
2020-10-30 00:14:15 +0300

    refactor

--- src/wait-events.ts
+++ src/wait-events.ts
@@ -6,16 +6,16 @@ export default function <T>(...sourses: [EventTarget, ...string[]][]) {
     const def = defer<T>();
 
     function onResolve() {
-        for (let [o, ...events] of sourses) {
-            for (let e of events) {
+        for (const [o, ...events] of sourses) {
+            for (const e of events) {
                 o.removeEventListener(e, onResolve);
             }
         }
         def.resolve(undefined);
     }
 
-    for (let [o, ...events] of sourses) {
-        for (let e of events) {
+    for (const [o, ...events] of sourses) {
+        for (const e of events) {
             o.addEventListener(e, onResolve, onceParams);
         }
     }
